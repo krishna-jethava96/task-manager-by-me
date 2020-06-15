d66ca41ecae5e88209359d94406f7fd3dae5bf26
krishna Jethava <krishnajethava.96@gmail.com>
2020-06-15 17:07:54 +0530

    async-await done

--- src/index.js
+++ src/index.js
@@ -38,34 +38,88 @@ app.get('/user/:id',(req,res) =>{
     })
 })
 
-app.post('/tasks',(req,res)=>{
+// why we use async-await bcaz async function default return a promise...so dont need to write again agai then() and catch()
+
+app.post('/tasks', async (req,res)=>{
     const task=new Task(req.body)
-
-    task.save().then(()=>{
+    try{
+        await task.save()
         res.status(201).send(task)
-    }).catch((e)=>{
+    }catch(e){
         res.status(400).send(e)
-    })
+    }
 })
 
-app.get('/tasks',(req,res) =>{
-    Task.find({}).then((users) =>{
-        res.send(users)
-    }).catch((e)=>{
+app.get('/tasks', async (req,res) =>{
+
+    try{
+        const tasks= await Task.find({})
+        res.status(200).send(tasks)
+    }catch(e){
         res.status(500).send()
-    })
+    }
+    // Task.find({}).then((users) =>{
+    //     res.send(users)
+    // }).catch((e)=>{
+    //     res.status(500).send()
+    // })
 })
 
-app.get('/task/:id',(req,res) =>{
-    const _id=req.params.id 
-    Task.findById(_id).then((task)=>{
+app.get('/task/:id',async (req,res) =>{
+    const _id=req.params.id
+    
+    try{
+        const task= await Task.findById(_id)
         if(!task){
             return res.status(404).send()
         }
-        res.send(task)
-    }).catch(()=>{
+        res.status(200).send(task)
+    }catch(e){
         res.status(500).send()
+    }
+    // Task.findById(_id).then((task)=>{
+    //     if(!task){
+    //         return res.status(404).send()
+    //     }
+    //     res.send(task)
+    // }).catch(()=>{
+    //     res.status(500).send()
+    // })
+})
+
+app.patch('/tasks/:id', async (req,res)=>{
+
+    const updates=Object.keys(req.body)
+    const allowedUpdates=['description','completed']
+    const isValidOperation=updates.every((update)=> {
+        return allowedUpdates.includes(update)
     })
+    if(!isValidOperation){
+        res.status(400).send({ Error: 'Invalid Updates..!' })
+    }
+
+    const _id=req.params.id
+    try{
+        const task= await Task.findByIdAndUpdate(_id,req.body,{new:true,runValidators:true})
+        if(!task){
+            res.status(404).send()
+        }
+        res.send(task)
+    }catch(e){
+        res.status(500).send()
+    }
+})
+
+app.delete('/task/:id', async (req,res) => {
+    try{
+        const task= await Task.findByIdAndDelete(req.params.id)
+        if(!task){
+            res.status(404).send()
+        }
+        res.send(task)
+    }catch(e){
+        res.status(500).send()
+    }
 })
 
 app.listen(port,() =>{
